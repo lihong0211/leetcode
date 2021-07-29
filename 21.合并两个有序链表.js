0c0cf32432b75be4f098cfc699e81991e42a1b67
lihong <lihong@bilibili.com>
2021-07-29 21:15:55 +0800

    feat: 合并链表

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -38,5 +38,22 @@ var mergeTwoLists = function(l1, l2) {
     })
     return head
 };
+
+const mergeTwoLists = (l1, l2) => {
+  let preHead = new ListNode(-1)
+  let pre = preHead
+  while(l1 && l2) {
+    if (l1.val > l2.val) {
+      pre.next = l2
+      l2 = l2.next
+    } else {
+      pre.next = l1
+      l1 = l1.next
+    }
+    pre = pre.next
+  }
+  pre.next = l1 ? l1 : l2
+  return preHead.next
+}
 // @lc code=end
 
